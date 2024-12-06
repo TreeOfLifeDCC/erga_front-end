f686907204d529b7c5326ccd95bdf8b0db3d3e5e
Mil-m <mansurova.m94@gmail.com>
2024-12-06 12:03:15 +0000

    abc-408 addition URL parameters for filters

--- src/app/status-tracking/status-tracking.component.ts
+++ src/app/status-tracking/status-tracking.component.ts
@@ -9,7 +9,6 @@ import {MatList, MatListItem} from "@angular/material/list";
 import {FlexModule} from "@angular/flex-layout";
 import {MatLine} from "@angular/material/core";
 import {MatChip, MatChipSet} from "@angular/material/chips";
-import {NgForOf} from "@angular/common";
 import {MatIcon} from "@angular/material/icon";
 import {MatProgressSpinner} from "@angular/material/progress-spinner";
 import {MatFormField, MatLabel} from "@angular/material/form-field";
@@ -26,7 +25,8 @@ import {RouterLink} from "@angular/router";
 import {MatAnchor} from "@angular/material/button";
 import {MatInput} from "@angular/material/input";
 import {HttpClient} from "@angular/common/http";
-import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
+
 
 @Component({
     selector: 'app-status-tracking',
@@ -97,32 +97,44 @@ export class StatusTrackingComponent implements OnInit, AfterViewInit {
     @ViewChild(MatSort, { static: true }) sort: MatSort;
     @ViewChild('input', { static: true }) searchInput: ElementRef;
 
-    constructor(private _apiService: ApiService,
-                private router: Router,
-                private activatedRoute: ActivatedRoute,) { }
+    constructor(
+        private _apiService: ApiService,
+        private activatedRoute: ActivatedRoute,
+    ) {}
 
     ngOnInit(): void {
         this.activatedRoute.queryParams.subscribe(params => {
             this.queryParams = {...params};
+
+            this.activeFilters = [];
+            this.phylogenyFilters = [];
+            this.currentClass = 'kingdom';
+
+            if ('filter' in this.queryParams){
+                this.activeFilters = Array.isArray(this.queryParams['filter']) ?
+                    [...this.queryParams['filter']] : [this.queryParams['filter']];
+            }
+            if (this.queryParams['sortActive'] && this.queryParams['sortDirection']){
+                this.sort.active = this.queryParams['sortActive'];
+                this.sort.direction = this.queryParams['sortDirection'];
+            }
+            if ('searchValue' in this.queryParams){
+                this.searchValue = this.queryParams['searchValue'];
+                this.searchInput.nativeElement.value = this.queryParams['searchValue'];
+            }
+            if ('pageIndex' in this.queryParams){
+                this.paginator.pageIndex = this.queryParams['pageIndex'];
+            }
+            if ('pageSize' in this.queryParams){
+                this.paginator.pageSize = this.queryParams['pageSize'];
+            }
+            if (this.phylogenyFilters.length > 0) {
+                this.queryParams['phylogenyFilters'] = this.phylogenyFilters.join(',');
+            }
+            if (this.currentClass && this.currentClass !== 'kingdom') {
+                this.queryParams['currentClass'] = this.currentClass;
+            }
         });
-        if ('filter' in this.queryParams){
-            this.activeFilters = Array.isArray(this.queryParams['filter']) ?
-                [...this.queryParams['filter']] : [this.queryParams['filter']];
-        }
-        if (this.queryParams['sortActive'] && this.queryParams['sortDirection']){
-            this.sort.active = this.queryParams['sortActive'];
-            this.sort.direction = this.queryParams['sortDirection'];
-        }
-        if ('searchValue' in this.queryParams){
-            this.searchValue = this.queryParams['searchValue'];
-            this.searchInput.nativeElement.value = this.queryParams['searchValue'];
-        }
-        if ('pageIndex' in this.queryParams){
-            this.paginator.pageIndex = this.queryParams['pageIndex'];
-        }
-        if ('pageSize' in this.queryParams){
-            this.paginator.pageSize = this.queryParams['pageSize'];
-        }
     }
 
     ngAfterViewInit() {
