ac658ead490650742de601246d897197c5d1b7e6
Mil-m <mansurova.m94@gmail.com>
2025-02-13 13:45:32 +0000

    abc-414 styles fixes for PR

--- src/app/data-portal/data-portal-details/data-portal-details.component.ts
+++ src/app/data-portal/data-portal-details/data-portal-details.component.ts
@@ -36,7 +36,7 @@ import {MatProgressSpinner} from "@angular/material/progress-spinner";
 import {MatAnchor, MatButton} from "@angular/material/button";
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
-import {MatChip} from "@angular/material/chips";
+import {MatChip, MatChipSet} from "@angular/material/chips";
 import {NgForOf, NgIf} from "@angular/common";
 import {FlexLayoutModule} from "@ngbracket/ngx-layout";
 import {MatTableExporterModule} from "mat-table-exporter";
@@ -101,7 +101,8 @@ import { MatIconModule } from '@angular/material/icon';
         TitleCasePipe,
         ScrollingModule,
         FlexLayoutModule,
-        MatIconModule
+        MatIconModule,
+        MatChipSet
     ],
     styleUrls: ['./data-portal-details.component.css']
 })
