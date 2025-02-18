fbcb70d76a1003d9fa0b4086774356c92909830b
Mil-m <mansurova.m94@gmail.com>
2025-02-18 16:10:45 +0000

    abc-414 specimens details page fix

--- src/app/organism-details/organism-details.component.ts
+++ src/app/organism-details/organism-details.component.ts
@@ -4,22 +4,18 @@ import {ApiService} from "../api.service";
 import {MatPaginator} from "@angular/material/paginator";
 import {MatSort} from "@angular/material/sort";
 import {
-    MatCell, MatCellDef,
-    MatColumnDef,
-    MatHeaderCell,
-    MatHeaderCellDef, MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+    MatCell,
+    MatCellDef, MatColumnDef, MatHeaderCell,
+    MatHeaderCellDef, MatHeaderRow,
+    MatHeaderRowDef, MatNoDataRow, MatRow,
+    MatRowDef,
     MatTable,
     MatTableDataSource
 } from "@angular/material/table";
 import {MatCard, MatCardActions, MatCardTitle} from "@angular/material/card";
-import {NgIf} from "@angular/common";
-import {MatDivider} from "@angular/material/divider";
 import {MatProgressSpinner} from "@angular/material/progress-spinner";
-import {MatFormField, MatLabel} from "@angular/material/form-field";
-import {MatChip} from "@angular/material/chips";
-import {MatAnchor} from "@angular/material/button";
-import {MatInput} from "@angular/material/input";
-import {FlexLayoutModule} from "@ngbracket/ngx-layout";
+import {NgForOf, NgIf} from "@angular/common";
+import {ImageSliderComponent} from "../image-slider/image-slider.component";
 
 @Component({
     selector: 'app-organism-details',
@@ -29,47 +25,43 @@ import {FlexLayoutModule} from "@ngbracket/ngx-layout";
         MatCard,
         MatCardTitle,
         MatCardActions,
-        NgIf,
-        MatDivider,
+        MatPaginator,
         MatProgressSpinner,
-
-        MatTable,
-        MatSort,
-        MatColumnDef,
-        MatChip,
-        MatHeaderCell,
-        MatHeaderCellDef,
-        MatCell,
-        MatCellDef,
         RouterLink,
-        MatAnchor,
-        MatInput,
+        MatTable,
+        NgIf,
+        NgForOf,
+        MatHeaderCellDef,
+        MatCellDef,
         MatHeaderRowDef,
+        MatRowDef,
+        MatNoDataRow,
+        MatColumnDef,
+        MatHeaderCell,
+        MatCell,
         MatHeaderRow,
         MatRow,
-        MatRowDef,
-        MatPaginator,
-        MatLabel,
-        MatFormField,
-        FlexLayoutModule
+        ImageSliderComponent
     ],
     styleUrls: ['./organism-details.component.css']
 })
 export class OrganismDetailsComponent implements OnInit, AfterViewInit {
     data: any;
     specimensData: any;
-    specimensDataLength: number;
-    specimensDisplayedColumns: string[] = ['accession', 'organism', 'commonName', 'organismPart', 'sex'];
+    specimensDataLength: number | undefined;
+    specimensDisplayedColumns: string[] = ['source', 'type', 'target'];
 
     resultsLength = 0;
     isLoadingResults = true;
     isRateLimitReached = false;
+    pageIndex: number = 0;
 
-    @ViewChild(MatPaginator) paginator: MatPaginator;
-    @ViewChild(MatSort) sort: MatSort;
+    slides: any[];
 
-    constructor(private route: ActivatedRoute, private _apiService: ApiService) {
-    }
+    @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+    @ViewChild(MatSort) sort: MatSort | undefined;
+
+    constructor(private route: ActivatedRoute, private _apiService: ApiService) { }
 
     ngOnInit(): void {
     }
@@ -77,20 +69,22 @@ export class OrganismDetailsComponent implements OnInit, AfterViewInit {
     ngAfterViewInit() {
         const routeParams = this.route.snapshot.paramMap;
         const organismId = routeParams.get('organismId');
-        this._apiService.getDetailsData(organismId, 'organisms_test').subscribe(
+        this._apiService.getDetailsData(organismId, 'specimens').subscribe(
             data => {
                 this.data = data['results'][0]['_source'];
                 this.isLoadingResults = false;
                 this.isRateLimitReached = data === null;
 
-                this.specimensData = new MatTableDataSource(data['results'][0]['_source']['specimens']);
-                if (data['results'][0]['_source']['specimens']) {
-                    this.specimensDataLength = data['results'][0]['_source']['specimens'].length;
+                this.specimensData = new MatTableDataSource(this.data['relationships']);
+                if (this.data['relationships']) {
+                    this.specimensDataLength = this.data['relationships'].length;
                 } else {
                     this.specimensDataLength = 0;
                 }
                 this.specimensData.paginator = this.paginator;
                 this.specimensData.sort = this.sort;
+
+                this.slides = this.generateSlides(this.data);
             }
         );
     }
@@ -103,8 +97,20 @@ export class OrganismDetailsComponent implements OnInit, AfterViewInit {
         }
     }
 
-    checkNagoyaProtocol(data: any): boolean {
-        return data.hasOwnProperty('nagoya_protocol');
-    }
+    private generateSlides(data: any) {
+        console.log(data);
+        const output = [];
+        const arr = data.images;
+        if (arr!== undefined) {
+            for (let i = 0; i < arr.length; i++) {
+                const obj = {url: encodeURI(arr[i])
+                        .replace('(', '%28')
+                        .replace(')', '%29')};
+                output.push(obj);
+            }
+        }
 
+        console.log(output);
+        return output;
+    }
 }
